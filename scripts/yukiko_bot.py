68651a0ffd318b8066b84748a1de829c45a48752
Shoji Ihara <ihara@spiber.jp>
2012-11-05 22:55:12 +0900

    add yukiko.py

--- scripts/yukiko_bot.py
+++ scripts/yukiko_bot.py
@@ -4,21 +4,11 @@ import os, sys
 path = os.path.abspath(os.path.dirname(__file__)) + '/..'
 sys.path.append(path)
 
-import Skype4Py
-import time
-from yukiko.handlers import tamaki_handler
-from yukiko.handlers import ihara_handler
-print tamaki_handler
+from yukiko import yukiko
 
 def main():
-    skype = Skype4Py.Skype() # Transport="x11"
-    skype.RegisterEventHandler('MessageStatus', tamaki_handler.handle)
-    skype.RegisterEventHandler('MessageStatus', ihara_handler.handle)
-    skype.Attach()
-    # イベントハンドラは別スレッドにて実行されるので、
-    # 本スレッドではひたすらsleepしてスクリプトが終了しないようにしておく。
-    while True:
-        time.sleep(1) 
+    _yukiko = yukiko.Yukiko()
+    _yukiko.maharagi()
 
 if __name__ == "__main__":
     main()
