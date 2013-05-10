fbe2d5b4053c282652043bf018c277e918b79d2f
webapp <webapp@www23427ue.sakura.ne.jp>
2013-05-11 00:11:02 +0900

    added increment/decrement

--- yukiko/handlers/tamaki_handler.py
+++ yukiko/handlers/tamaki_handler.py
@@ -1,9 +1,27 @@
 #-*- coding:utf-8 -*-
 
+import sys
 import re
 import urllib2
 import BeautifulSoup
+try:
+    import cPickle as pickle
+except:
+	  import pickle
+
+
+try:
+    f = file('data/score.dump', 'r')
+    score = pickle.load(f)
+    f.close()
+except:
+    score = {}
+
 timeout = 10
+
+inc = re.compile(r'(\S+)\+\+')
+dec = re.compile(r'(\S+)\-\-')
+
 def handle(msg, event):
     if event == u"RECEIVED":
         if re.search(u'ガンダム', msg.Body):
@@ -16,3 +34,23 @@ def handle(msg, event):
                 )
         elif re.match(u'\#V-MAX', msg.Body):
             msg.Chat.SendMessage(u"レディ")
+
+        incmatch = inc.search(msg.Body)
+        if incmatch:
+            print incmatch.group(1)
+            score.setdefault(incmatch.group(1), 0)
+            score[incmatch.group(1)] += 1
+            f2 = file('data/score.dump', 'w')
+            pickle.dump(score, f2)
+            f2.close()
+            msg.Chat.SendMessage(u'( ' + incmatch.group(1) + u': 累計 ' + str(score[incmatch.group(1)]) + u' )' )
+
+        decmatch = dec.search(msg.Body)
+        if decmatch:
+            print decmatch.group(1) 
+            score.setdefault(decmatch.group(1), 0)
+            score[decmatch.group(1)] -= 1
+            f2 = file('data/score.dump', 'w')
+            pickle.dump(score, f2)
+            f2.close()
+            msg.Chat.SendMessage(u'( ' + decmatch.group(1) + u': 累計 ' + str(score[decmatch.group(1)]) + u' )' )
