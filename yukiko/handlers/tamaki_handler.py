992c7acf307b2512cff74dac95704520d40f3baa
coela <coela.st@gmail.com>
2013-05-12 09:11:05 +0900

    fixed decoding problems ...again

--- yukiko/handlers/tamaki_handler.py
+++ yukiko/handlers/tamaki_handler.py
@@ -3,7 +3,7 @@
 import re
 import urllib2
 import BeautifulSoup
-
+import xml.parsers.expat
 timeout = 10
 
 KNOWN_ENCODINGS = ["cp932", "shift_jis_2004", "euc-jp-ms",
@@ -26,7 +26,7 @@ def handle(msg, event):
 
             enc = soup.originalEncoding
             title = soup.title.string
-            hoge = re.sub(r'\&\#(\d+)\;',convert_ascii,title).replace("\n",'')
+            hoge = unescape(title)
             msg.Chat.SendMessage(
                     hoge
                )
@@ -44,3 +44,32 @@ def testKnownEncodings(html):
       return soup
     except (TypeError, UnicodeError):
       pass
+
+
+def unescape(s):
+    want_unicode = False
+    if isinstance(s, unicode):
+        s = s.encode("utf-8")
+        want_unicode = True
+
+    # the rest of this assumes that `s` is UTF-8
+    list = []
+
+    # create and initialize a parser object
+    p = xml.parsers.expat.ParserCreate("utf-8")
+    p.buffer_text = True
+    p.returns_unicode = want_unicode
+    p.CharacterDataHandler = list.append
+
+    # parse the data wrapped in a dummy element
+    # (needed so the "document" is well-formed)
+    p.Parse("<e>", 0)
+    p.Parse(s, 0)
+    p.Parse("</e>", 1)
+
+    # join the extracted strings and return
+    es = ""
+    if want_unicode:
+        es = u""
+    return es.join(list)
+
