fdb983a0c2555e87bd40d7f543f8c7a128c70913
coela <coela.st@gmail.com>
2013-05-12 09:38:16 +0900

    stop using unescape function. unescape function seems not working against some tweets

--- yukiko/handlers/twitter_handler.py
+++ yukiko/handlers/twitter_handler.py
@@ -2,7 +2,6 @@
 import re
 import urllib2
 import BeautifulSoup
-import xml.parsers.expat
 
 timeout = 10
 
@@ -17,36 +16,9 @@ def handle(msg, event):
             twitter_textArray = twitter_context.find("p", attrs={"class": "js-tweet-text"})
             twitter_text = u''.join(twitter_textArray.findAll(text=True))
             twitter_time = twitter_context.find("a",attrs={"class": "tweet-timestamp js-permalink js-nav"})['title']
-            hoge = unescape(twitter_text)
+            hoge = twitter_text.replace('&#10;',"\n")
             msg.Chat.SendMessage(
                 hoge + u'\n[' + twitter_time + ']'
                 )
 
-
-def unescape(s):
-    want_unicode = False
-    if isinstance(s, unicode):
-        s = s.encode("utf-8")
-        want_unicode = True
-
-    # the rest of this assumes that `s` is UTF-8
-    list = []
-
-    # create and initialize a parser object
-    p = xml.parsers.expat.ParserCreate("utf-8")
-    p.buffer_text = True
-    p.returns_unicode = want_unicode
-    p.CharacterDataHandler = list.append
-
-    # parse the data wrapped in a dummy element
-    # (needed so the "document" is well-formed)
-    p.Parse("<e>", 0)
-    p.Parse(s, 0)
-    p.Parse("</e>", 1)
-
-    # join the extracted strings and return
-    es = ""
-    if want_unicode:
-        es = u""
-    return es.join(list)
 
