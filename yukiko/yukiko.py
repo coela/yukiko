c584914746662b8c41a28ac4d987ad1039faf0ed
coela <coela.st@gmail.com>
2013-05-14 18:38:31 +0900

    added codon handler

--- yukiko/yukiko.py
+++ yukiko/yukiko.py
@@ -4,6 +4,7 @@ from handlers import incdec_handler
 from handlers import twitter_handler
 from handlers import tamaki_handler
 from handlers import homeru_handler
+from handlers import codon_handler
 from handlers import ihara_handler
 
 class Yukiko:
@@ -15,6 +16,7 @@ class Yukiko:
         skype.RegisterEventHandler('MessageStatus', twitter_handler.handle)
         skype.RegisterEventHandler('MessageStatus', incdec_handler.handle)
         skype.RegisterEventHandler('MessageStatus', homeru_handler.handle)
+        skype.RegisterEventHandler('MessageStatus', codon_handler.handle)
 #       skype.RegisterEventHandler('MessageStatus', ihara_handler.handle)
         skype.Attach()
         while True:
