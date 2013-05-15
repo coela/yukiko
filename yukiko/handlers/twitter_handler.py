4e1fa72ae1ccf2f7fdeca88a3b9e9fe21b890ce4
coela <coela.st@gmail.com>
2013-05-15 19:02:32 +0900

    chaged class name to find tweet

--- yukiko/handlers/twitter_handler.py
+++ yukiko/handlers/twitter_handler.py
@@ -8,16 +8,16 @@ timeout = 10
 
 def handle(msg, event):
     if event == u"RECEIVED":
-        if re.match(u'https://twitter.com/[^\/]+/status/', msg.Body):
+        if re.match(u'http[s]*://twitter.com/[^\/]+/status/', msg.Body):
             opener = urllib2.build_opener()
-            opener.addheaders = [('user-agent', 'mozilla/5.0')]
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
             html = opener.open(msg.Body,None,timeout)
             soup = BeautifulSoup.BeautifulSoup(html,convertEntities=BeautifulSoup.BeautifulSoup.HTML_ENTITIES)
             twitter_context = soup.find("div", attrs={"class": "content"})
-            twitter_textArray = twitter_context.find("p", attrs={"class": "js-tweet-text"})
-            twitter_text = u''.join(twitter_textArray.findAll(text=True))
+            twitter_text_array = twitter_context.find("p", attrs={"class": "js-tweet-text tweet-text"})
+            twitter_text = u''.join(twitter_text_array.findAll(text=True))
             twitter_time = twitter_context.find("a",attrs={"class": "tweet-timestamp js-permalink js-nav"})['title']
             msg.Chat.SendMessage(
-                twitter_text + u'\n[' + twitter_time + ']'
+                twitter_text + u"\n[" + twitter_time + u']'
                 )
 
